1bc502e53f9cfbbef7d488d6bad14f78e19252e0
bbr46303-cell <bbr46303@gmail.com>
2026-08-26 17:28:44 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("."));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
